db828651bba2090190ffca17a03259ad54142f59
kvchaitanyasaivarma <kvchaitanyavarma@gmail.com>
2022-04-24 02:21:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# snipper-rnd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-ptomll)
---
+++ src/main.js
@@ -0,0 +1,6 @@
+const { createApp } = require('vue');
+import App from "./App.vue";
+import Header from "./Header.vue";
+
+createApp(App).mount("#app");
+createApp(Header).mount("#header");
